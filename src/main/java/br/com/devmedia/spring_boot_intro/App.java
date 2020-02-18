966969285bc2890fe8ca5f0ab7c0916f9260618a
matheuspessoa <matheuspessoa@gmail.com>
2020-02-18 15:18:25 -0300

    Starting the project

---
+++ src/main/java/br/com/devmedia/spring_boot_intro/App.java
@@ -0,0 +1,13 @@
+package br.com.devmedia.spring_boot_intro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
